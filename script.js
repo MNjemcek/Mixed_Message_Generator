92c1c644549d1ee7181dc81f271a19c3ef7959f7
Marian Njemcek <marian1njemcek1@gmail.com>
2022-05-10 18:47:43 +0200

    Created message output

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const countries = require("./data.js"); //Imports data from data.js file
 
+//function that generate random Integer between 0 and inputNumber - 1
 const randIndex = inputNumber => {
     return Math.floor(Math.random() * inputNumber)
 }
+
+let randomCountry = countries[randIndex(countries.length)];
+
+console.log(`\nLet's talk about: ${randomCountry.name}\t
+    -Capital: ${randomCountry.capital}\t
+    -Population: ${randomCountry.population}\t
+    -Member of EU: ${randomCountry.EuMember}\n`);
